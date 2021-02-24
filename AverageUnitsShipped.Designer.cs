98b8623badb9a7500f8e85a6d7e7ab9d1cabfb54
Jacob Kent <jacobgkent@gmail.com>
2021-02-24 17:27:18 -0500

    Arrays implemented, SetDefault Implemented,
    TODO:
    Implement Average Calculations

--- AverageUnitsShipped.Designer.cs
+++ AverageUnitsShipped.Designer.cs
@@ -33,20 +33,20 @@ namespace Lab_1
             this.labelUnitsPrompt = new System.Windows.Forms.Label();
             this.textboxUnitsShipped = new System.Windows.Forms.TextBox();
             this.labelDay = new System.Windows.Forms.Label();
-            this.textboxPastEntries = new System.Windows.Forms.TextBox();
+            this.textBoxPastEntries1 = new System.Windows.Forms.TextBox();
             this.labelAverageOutput = new System.Windows.Forms.Label();
             this.buttonEnter = new System.Windows.Forms.Button();
             this.buttonReset = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBoxPastEntries2 = new System.Windows.Forms.TextBox();
+            this.textBoxPastEntries3 = new System.Windows.Forms.TextBox();
+            this.labelEmployeeAverage1 = new System.Windows.Forms.Label();
+            this.labelEmployeeAverage2 = new System.Windows.Forms.Label();
+            this.labelEmployeeAverage3 = new System.Windows.Forms.Label();
             this.labelEmployee1 = new System.Windows.Forms.Label();
             this.labelEmployee2 = new System.Windows.Forms.Label();
             this.labelEmployee3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelUnitsPrompt
@@ -74,27 +74,28 @@ namespace Lab_1
             this.labelDay.TabIndex = 2;
             this.labelDay.Text = "Day 1";
             this.labelDay.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolTip1.SetToolTip(this.labelDay, "Current day for entry.");
             // 
-            // textboxPastEntries
+            // textBoxPastEntries1
             // 
-            this.textboxPastEntries.BackColor = System.Drawing.SystemColors.Window;
-            this.textboxPastEntries.Location = new System.Drawing.Point(21, 107);
-            this.textboxPastEntries.Multiline = true;
-            this.textboxPastEntries.Name = "textboxPastEntries";
-            this.textboxPastEntries.ReadOnly = true;
-            this.textboxPastEntries.Size = new System.Drawing.Size(115, 116);
-            this.textboxPastEntries.TabIndex = 3;
-            this.toolTip1.SetToolTip(this.textboxPastEntries, "Past entries for this week");
+            this.textBoxPastEntries1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPastEntries1.Location = new System.Drawing.Point(21, 107);
+            this.textBoxPastEntries1.Multiline = true;
+            this.textBoxPastEntries1.Name = "textBoxPastEntries1";
+            this.textBoxPastEntries1.ReadOnly = true;
+            this.textBoxPastEntries1.Size = new System.Drawing.Size(115, 116);
+            this.textBoxPastEntries1.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.textBoxPastEntries1, "Past entries for Employee 1 this week.");
             // 
             // labelAverageOutput
             // 
-            this.labelAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelAverageOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.labelAverageOutput.Location = new System.Drawing.Point(21, 254);
             this.labelAverageOutput.Name = "labelAverageOutput";
             this.labelAverageOutput.Size = new System.Drawing.Size(357, 23);
             this.labelAverageOutput.TabIndex = 4;
             this.labelAverageOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.toolTip1.SetToolTip(this.labelAverageOutput, "Average number of units shipped for this week");
+            this.toolTip1.SetToolTip(this.labelAverageOutput, "Average number of units shipped by all employees for this week.");
             // 
             // buttonEnter
             // 
@@ -114,7 +115,7 @@ namespace Lab_1
             this.buttonReset.Size = new System.Drawing.Size(115, 23);
             this.buttonReset.TabIndex = 6;
             this.buttonReset.Text = "&Reset";
-            this.toolTip1.SetToolTip(this.buttonReset, "Reset the program. Clearing the current entry and past entries.");
+            this.toolTip1.SetToolTip(this.buttonReset, "Reset the program. Clearing the current entry and ALL past entries.");
             this.buttonReset.UseVisualStyleBackColor = true;
             this.buttonReset.Click += new System.EventHandler(this.ButtonResetClick);
             // 
@@ -128,6 +129,58 @@ namespace Lab_1
             this.toolTip1.SetToolTip(this.buttonExit, "Exit the program");
             this.buttonExit.UseVisualStyleBackColor = true;
             this.buttonExit.Click += new System.EventHandler(this.ButtonExitClick);
+            // 
+            // textBoxPastEntries2
+            // 
+            this.textBoxPastEntries2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPastEntries2.Location = new System.Drawing.Point(142, 107);
+            this.textBoxPastEntries2.Multiline = true;
+            this.textBoxPastEntries2.Name = "textBoxPastEntries2";
+            this.textBoxPastEntries2.ReadOnly = true;
+            this.textBoxPastEntries2.Size = new System.Drawing.Size(115, 116);
+            this.textBoxPastEntries2.TabIndex = 11;
+            this.toolTip1.SetToolTip(this.textBoxPastEntries2, "Past entries for Employee 2 this week.");
+            // 
+            // textBoxPastEntries3
+            // 
+            this.textBoxPastEntries3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxPastEntries3.Location = new System.Drawing.Point(263, 107);
+            this.textBoxPastEntries3.Multiline = true;
+            this.textBoxPastEntries3.Name = "textBoxPastEntries3";
+            this.textBoxPastEntries3.ReadOnly = true;
+            this.textBoxPastEntries3.Size = new System.Drawing.Size(115, 116);
+            this.textBoxPastEntries3.TabIndex = 12;
+            this.toolTip1.SetToolTip(this.textBoxPastEntries3, "Past entries for Employee 3 this week.");
+            // 
+            // labelEmployeeAverage1
+            // 
+            this.labelEmployeeAverage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployeeAverage1.Location = new System.Drawing.Point(21, 226);
+            this.labelEmployeeAverage1.Name = "labelEmployeeAverage1";
+            this.labelEmployeeAverage1.Size = new System.Drawing.Size(115, 23);
+            this.labelEmployeeAverage1.TabIndex = 13;
+            this.labelEmployeeAverage1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployeeAverage1, "Average number of units shipped for this week");
+            // 
+            // labelEmployeeAverage2
+            // 
+            this.labelEmployeeAverage2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployeeAverage2.Location = new System.Drawing.Point(142, 226);
+            this.labelEmployeeAverage2.Name = "labelEmployeeAverage2";
+            this.labelEmployeeAverage2.Size = new System.Drawing.Size(115, 23);
+            this.labelEmployeeAverage2.TabIndex = 14;
+            this.labelEmployeeAverage2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployeeAverage2, "Average number of units shipped by Employee 2 for this week.\r\n");
+            // 
+            // labelEmployeeAverage3
+            // 
+            this.labelEmployeeAverage3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelEmployeeAverage3.Location = new System.Drawing.Point(263, 226);
+            this.labelEmployeeAverage3.Name = "labelEmployeeAverage3";
+            this.labelEmployeeAverage3.Size = new System.Drawing.Size(115, 23);
+            this.labelEmployeeAverage3.TabIndex = 15;
+            this.labelEmployeeAverage3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip1.SetToolTip(this.labelEmployeeAverage3, "Average number of units shipped by Employee 3 for this week.");
             // 
             // labelEmployee1
             // 
@@ -156,58 +209,6 @@ namespace Lab_1
             this.labelEmployee3.TabIndex = 10;
             this.labelEmployee3.Text = "Employee 3";
             // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(142, 107);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(115, 116);
-            this.textBox1.TabIndex = 11;
-            this.toolTip1.SetToolTip(this.textBox1, "Past entries for this week");
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(263, 107);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(115, 116);
-            this.textBox2.TabIndex = 12;
-            this.toolTip1.SetToolTip(this.textBox2, "Past entries for this week");
-            // 
-            // label1
-            // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(21, 226);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 23);
-            this.label1.TabIndex = 13;
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.toolTip1.SetToolTip(this.label1, "Average number of units shipped for this week");
-            // 
-            // label2
-            // 
-            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(142, 226);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(115, 23);
-            this.label2.TabIndex = 14;
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.toolTip1.SetToolTip(this.label2, "Average number of units shipped for this week");
-            // 
-            // label3
-            // 
-            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(263, 226);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 23);
-            this.label3.TabIndex = 15;
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.toolTip1.SetToolTip(this.label3, "Average number of units shipped for this week");
-            // 
             // formAverageUnitsShipped
             // 
             this.AcceptButton = this.buttonEnter;
@@ -215,11 +216,11 @@ namespace Lab_1
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonReset;
             this.ClientSize = new System.Drawing.Size(400, 331);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelEmployeeAverage3);
+            this.Controls.Add(this.labelEmployeeAverage2);
+            this.Controls.Add(this.labelEmployeeAverage1);
+            this.Controls.Add(this.textBoxPastEntries3);
+            this.Controls.Add(this.textBoxPastEntries2);
             this.Controls.Add(this.labelEmployee3);
             this.Controls.Add(this.labelEmployee2);
             this.Controls.Add(this.labelEmployee1);
@@ -227,7 +228,7 @@ namespace Lab_1
             this.Controls.Add(this.buttonReset);
             this.Controls.Add(this.buttonEnter);
             this.Controls.Add(this.labelAverageOutput);
-            this.Controls.Add(this.textboxPastEntries);
+            this.Controls.Add(this.textBoxPastEntries1);
             this.Controls.Add(this.labelDay);
             this.Controls.Add(this.textboxUnitsShipped);
             this.Controls.Add(this.labelUnitsPrompt);
@@ -246,7 +247,7 @@ namespace Lab_1
         private System.Windows.Forms.Label labelUnitsPrompt;
         private System.Windows.Forms.TextBox textboxUnitsShipped;
         private System.Windows.Forms.Label labelDay;
-        private System.Windows.Forms.TextBox textboxPastEntries;
+        private System.Windows.Forms.TextBox textBoxPastEntries1;
         private System.Windows.Forms.Label labelAverageOutput;
         private System.Windows.Forms.Button buttonEnter;
         private System.Windows.Forms.Button buttonReset;
@@ -255,11 +256,11 @@ namespace Lab_1
         private System.Windows.Forms.Label labelEmployee1;
         private System.Windows.Forms.Label labelEmployee2;
         private System.Windows.Forms.Label labelEmployee3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPastEntries2;
+        private System.Windows.Forms.TextBox textBoxPastEntries3;
+        private System.Windows.Forms.Label labelEmployeeAverage1;
+        private System.Windows.Forms.Label labelEmployeeAverage2;
+        private System.Windows.Forms.Label labelEmployeeAverage3;
     }
 }
 
--- AverageUnitsShipped.cs
+++ AverageUnitsShipped.cs
@@ -17,11 +17,73 @@ namespace Lab_1
 {
     public partial class formAverageUnitsShipped : Form
     {
+
+        // Declarations
         int currentDay = 1;
         int runningTotal = 0;
+        int currentEmployee = 0;
+
+        // Arrays
+        int[,] unitsShippedArray = new int[3, 7];
+
+        TextBox[] pastEntriesArray;
+        Label[] averageLabelArray;
+
         public formAverageUnitsShipped()
         {
             InitializeComponent();
+
+            // Arrays of the textboxes and labels
+            pastEntriesArray = new TextBox[] { textBoxPastEntries1, textBoxPastEntries2, textBoxPastEntries3 };
+            averageLabelArray = new Label[] { labelEmployeeAverage1, labelEmployeeAverage2, labelEmployeeAverage3 };
+
+            SetDefaults();
+        }
+
+
+        /// <summary>
+        /// Function to return the form to its default state.
+        /// </summary>
+        private void SetDefaults()
+        {
+            // Clear all input and output fields
+            textboxUnitsShipped.Clear();
+            ClearControls(pastEntriesArray);
+            ClearControls(averageLabelArray);
+                    // textBoxPastEntries1.Clear();
+                    // textBoxPastEntries2.Clear();
+                    // textBoxPastEntries3.Clear();
+                    // labelEmployeeAverage1.Text = String.Empty;
+                    // labelEmployeeAverage2.Text = String.Empty;
+                    // labelEmployeeAverage3.Text = String.Empty;
+            labelAverageOutput.Text = String.Empty;
+
+            // Reset the global variables
+            currentDay = 1;
+            runningTotal = 0;
+            int currentEmployee = 0;
+
+            // Clear all contents of the unitsShippedArray
+            Array.Clear(unitsShippedArray, 0, unitsShippedArray.Length);
+
+            // Re-enable controls
+            textboxUnitsShipped.Enabled = true;
+            buttonEnter.Enabled = true;
+
+            // Set focus
+            textboxUnitsShipped.Focus();
+        }
+
+        /// <summary>
+        /// This clears the text property of all controls in the array of controls that is passed in
+        /// </summary>
+        /// <param name="controlArray">An array of controls with a text property to clear</param>
+        private void ClearControls(Control[] controlArray)
+        {
+            foreach (Control controlToClear in controlArray)
+            {
+                controlToClear.Text = String.Empty;
+            }
         }
 
         /// <summary>
@@ -41,22 +103,9 @@ namespace Lab_1
         /// <param name="e"></param>
         private void ButtonResetClick(object sender, EventArgs e)
         {
-            // Clear input feilds
-            textboxUnitsShipped.Clear();
-            textboxPastEntries.Clear();
-            labelAverageOutput.Text = String.Empty;
-
-            // Reset the global variables
-            currentDay = 1;
-            runningTotal = 0;
-
-            // Re-enable controls
-            textboxUnitsShipped.Enabled = true;
-            buttonEnter.Enabled = true;
-
-            // Set focus
-            textboxUnitsShipped.Focus();
+            SetDefaults();
         }
+
         /// <summary>
         /// Functionaly for entering a value. Including validation of the entry and the final calculation and output of the average.
         /// </summary>
@@ -68,6 +117,7 @@ namespace Lab_1
             const int MinimumUnits = 0;
             const int MaximumUnits = 5000;
             const int MaximumDays = 7;
+            const int TotalEmployees = 3;
             int lastEntry;
 
             // Validate that the number entered is a whole number
@@ -76,10 +126,13 @@ namespace Lab_1
                 // Validate that the number entered is between 0 and 5000
                 if (lastEntry >= MinimumUnits && lastEntry <= MaximumUnits)
                 {
-                    // Add the newest entry to the list of past entries
-                    textboxPastEntries.Text += lastEntry + "\r\n";
-                    // Add to the current total by the entry
-                    runningTotal += lastEntry;
+                    // Add the newest entry to the list of past entries for the current employee
+                    pastEntriesArray[currentEmployee].Text += lastEntry + "\r\n";
+                            // textBoxPastEntries1.Text += lastEntry + "\r\n";
+                    // Add the last entry to the unitsShippedArray for the current employee and day
+                    unitsShippedArray[currentEmployee, currentDay - 1] = lastEntry;
+                            // Add to the current total by the entry
+                            //runningTotal += lastEntry;
                     // Increment the day by 1
                     currentDay++;
                     // Clear the entry textbox after each valid entry
@@ -88,14 +141,25 @@ namespace Lab_1
                     // If the day is > 7
                     if (currentDay > MaximumDays)
                     {
+
+                        // For loop here to determine current employee average
+
+
+
+                        // Reset the current day
+                        currentDay = 1;
+                        // Increment to the next employee
+                        currentEmployee++;
+
+
                         // Determine the average
-                        double averageUnits;
-                        averageUnits = (double)runningTotal / (double)MaximumDays;
-                        // Output the result
-                        labelAverageOutput.Text = "Average per day: " + Math.Round(averageUnits, 2);
-                        // Disable controls
-                        textboxUnitsShipped.Enabled = false;
-                        buttonEnter.Enabled = false;
+                        // double averageUnits;
+                        // averageUnits = (double)runningTotal / (double)MaximumDays;
+                            // Output the result
+                        // labelAverageOutput.Text = "Average per day: " + Math.Round(averageUnits, 2);
+                            // Disable controls
+                        // textboxUnitsShipped.Enabled = false;
+                        // buttonEnter.Enabled = false;
 
                     }
                     // If the day is <= 7
